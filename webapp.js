7a5b8a17002a14ad28eb884a7114be438806526f
bats31 <tobias_schlauch@gmx.de>
2021-05-21 16:29:49 +0200

    change webapp.js

--- webapp.js
+++ webapp.js
@@ -1,24 +1,15 @@
 const http = require("http");
-const puppeteer = require("puppeteer");
 
 //const host = 'https://lbbawebtest1.azurewebsites.net';
 const port = process.env.PORT || 800;
  
  
 http.createServer(function (req, res) {
- 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
-
-  await browser.close()
-
 
   // HTTP response header - the content will be HTML MIME type
   res.writeHead(200, { 'Content-Type': "text/html" });
   // Write out the HTTP response body
-  res.write("@@TEST@@");
+  res.write("@@STR@@");
  
   // End of HTTP response
   res.end();
